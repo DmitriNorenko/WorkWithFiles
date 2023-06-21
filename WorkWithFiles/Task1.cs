1055b5b4d1ecb8a95d80c16cc5775790d5ca918e
Name <email@exaple.ru>
2023-06-21 11:27:07 +0300

    Задание1.

--- WorkWithFiles/Task1.cs
+++ WorkWithFiles/Task1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,22 @@ namespace WorkWithFiles
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string str = "/Tests";
+                DirectoryInfo directoryInfo = new DirectoryInfo(str);
 
+                if (directoryInfo.Exists)
+                {
+                    directoryInfo.Delete(true);
+                    Console.WriteLine("Старые файлы и папки удалены.");
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"У вас нет доступа.{ex}");
+            }
         }
     }
 }
